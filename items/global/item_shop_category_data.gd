2d8fed02e58797305e8cc84119b37a4d10bf8cb2
Glauber Nespoli <glauber.nespoli@gmail.com>
2023-01-09 12:22:24 +0100

    adding item categories

---
+++ items/global/item_shop_category_data.gd
@@ -0,0 +1,5 @@
+class_name ItemShopCategoryData
+extends ShopCategoryData
+
+func get_category()->int:
+	return Category.ITEM
---
+++ items/global/shop_category_data.gd
@@ -0,0 +1,9 @@
+class_name ShopCategoryData
+extends Resource
+
+export (String) var shop_category_id = ""
+export (Resource) var icon
+export (String) var name = ""
+
+func get_category()->int:
+	return - 1
